31637222c91086cbf41f8da4bc511510b49ca26c
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-07-16 01:20:35 -0300

    Added some content to "page_home.php"

--- page_home.php
+++ page_home.php
@@ -17,6 +17,18 @@
 		<link rel="shortcut icon" href="images/hanabi.png" type="image/x-icon">
 	</head>
 	<body>
-		<h3>PÁGINA INICIAL</h3>
+		<section>
+			<h3>PÁGINA&nbsp;INICIAL</h3>
+			<article>
+				<div>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
+			</article>
+		</section>
 	</body>
 </html>
