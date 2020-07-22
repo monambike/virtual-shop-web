73eb71ef560c72c64ae939c7f8cfb7ff62441313
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-07-22 01:16:11 -0300

    #21 Added Initial Product Divs

--- page_products.php
+++ page_products.php
@@ -6,6 +6,52 @@
 		<link rel="stylesheet" type="text/css" href="css/page_products.css">
 	</head>
 	<body>
-		<h3>NOSSOS&nbsp;PRODUTOS</h3>
+		<section>
+			<h3>NOSSOS&nbsp;PRODUTOS</h3>
+			<div class="item-row">
+				<article class="item-product">
+					<img src="" width="100%">
+					<h4>PRODUTO</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</article>
+				<article class="item-product">
+					<img src="" width="100%">
+					<h4>PRODUTO</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</article>
+			</div>
+			<div class="item-row">
+				<article class="item-product">
+					<img src="" width="100%">
+					<h4>PRODUTO</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</article>
+				<article class="item-product">
+					<img src="" width="100%">
+					<h4>PRODUTO</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</article>
+			</div>			
+		</section>
 	</body>
 </html>
