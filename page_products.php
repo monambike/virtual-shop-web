9b543e1f384eff39da21d8a2fc723367cc2c15f4
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-08-08 03:05:26 -0300

    Update products layout page
    - Added images;
    - Added padding;
    - Updated title;
    - Updated colors.

--- page_products.php
+++ page_products.php
@@ -9,8 +9,7 @@
 		<section>
 			<h3>NOSSOS&nbsp;PRODUTOS</h3>
 			<div class="item-row">
-				<article class="item-product">
-					<img src="" width="100%">
+				<article class="item-product item-1">
 					<h4>PRODUTO</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -18,9 +17,9 @@
 					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<img src="images/product-1.jpg" width="100%" height="200px">
 				</article>
-				<article class="item-product">
-					<img src="" width="100%">
+				<article class="item-product item-2">
 					<h4>PRODUTO</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -28,11 +27,11 @@
 					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<img src="images/product-2.jpg" width="100%" height="200px">
 				</article>
 			</div>
 			<div class="item-row">
-				<article class="item-product">
-					<img src="" width="100%">
+				<article class="item-product item-3">
 					<h4>PRODUTO</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -40,9 +39,9 @@
 					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<img src="images/product-3.jpg" width="100%" height="200px">
 				</article>
-				<article class="item-product">
-					<img src="" width="100%">
+				<article class="item-product item-4">
 					<h4>PRODUTO</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -50,6 +49,7 @@
 					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<img src="images/product-4.jpg" width="100%" height="200px">
 				</article>
 			</div>			
 		</section>
